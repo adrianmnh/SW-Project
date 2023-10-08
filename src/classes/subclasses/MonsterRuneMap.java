b0dd957418a5eac09d0cda06c2a70f12d317d5da
Adrian Legion <nlawliet6@gmail.com>
2023-10-07 23:28:15 -0400

    Monster Rune Map -> used to track the user summons and runes engraved on them. **Huge Bug** : only some(half of the items) are given a rune bag

--- src/classes/subclasses/MonsterRuneMap.java
+++ src/classes/subclasses/MonsterRuneMap.java
@@ -1,43 +1,78 @@
 package classes.subclasses;
 
-import classes.Monster;
 import runes.Rune;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class MonsterRuneMap extends HashMap<Monster, Set<Rune>> {
+public class MonsterRuneMap extends TreeMap<MonsterImageIcon, RuneSet> {
     public MonsterRuneMap(){
         super();
+
     }
-//    @Override
-    public Set<Rune> get(int key){
-        for(Monster monster : this.keySet()){
-            if(monster.getBaseId() == key){
+
+    public void addSummon(MonsterImageIcon monster){
+        RuneSet runeSet = new RuneSet();
+        this.put(monster, runeSet);
+        System.out.println("MonsterRuneMap: addSummon: " + monster.summonId + " new RuneSet->>>>" + runeSet);
+    }
+
+    public void addSummon(MonsterImageIcon monster, RuneSet runeSet){
+        this.put(monster, runeSet);
+    }
+
+    public RuneSet getBySummonId(int summonId){
+        for(MonsterImageIcon monster : this.keySet()){
+            if(monster.summonId == summonId){
                 return this.get(monster);
             }
         }
         return null;
     }
 
-
-
-    public String toString(){
-        System.out.println("RuneMap:");
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for(Monster monster : this.keySet()){
-            int id = monster.getBaseId();
-            stringBuilder.append("MonsterId: " + id + " - {");
-            for(Rune r : this.get(id)){
-                stringBuilder.append(r.runeId + " ");
+    public List<MonsterImageIcon> getSortedByBaseID(){
+        //sort by baseId
+        Stream<MonsterImageIcon> sorted = this.keySet().stream().sorted((o1, o2) -> {
+            if(o1.baseId > o2.baseId){
+                return 1;
+            }else if(o1.baseId < o2.baseId){
+                return -1;
+            }else{
+                return 0;
             }
-            stringBuilder.append("}\n");
-        }
-        return stringBuilder.toString();
+        });
+
+
+        return sorted.collect(Collectors.toList());
     }
 
-    public boolean runeIn(int monsterId, Rune r){
-        return this.get(monsterId).contains(r);
+    public String toString(){
+        System.out.println("RuneMap:\n{");
+        StringBuilder sb = new StringBuilder();
+
+        for(MonsterImageIcon monsterImageIcon : this.keySet()){
+            int id = monsterImageIcon.summonId;
+            sb.append("MonsterImageIcon " + monsterImageIcon.monster.getSummonId());
+            sb.append(" - {");
+            sb.append(this.get(monsterImageIcon) + " " );
+            sb.append("\t}\n");
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
+    public void printKeySet(){
+        System.out.println("KeySet: ");
+        for(MonsterImageIcon monsterImageIcon : this.keySet()){
+            System.out.println(monsterImageIcon);
+            System.out.println(this.get(monsterImageIcon));
+        }
+    }
+
+    public boolean runeIn(int summonId, Rune r){
+        return this.getBySummonId(summonId).contains(r);
     }
 }
---
+++ src/classes/subclasses/MonsterRuneMapOld.java
@@ -0,0 +1,43 @@
+package classes.subclasses;
+
+import classes.Monster;
+import runes.Rune;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class MonsterRuneMapOld extends HashMap<Monster, Set<Rune>> {
+    public MonsterRuneMapOld(){
+        super();
+    }
+//    @Override
+    public Set<Rune> get(int key){
+        for(Monster monster : this.keySet()){
+            if(monster.getBaseId() == key){
+                return this.get(monster);
+            }
+        }
+        return null;
+    }
+
+
+
+    public String toString(){
+        System.out.println("RuneMap:");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(Monster monster : this.keySet()){
+            int id = monster.getBaseId();
+            stringBuilder.append("MonsterId: " + id + " - {");
+            for(Rune r : this.get(id)){
+                stringBuilder.append(r.runeId + " ");
+            }
+            stringBuilder.append("}\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    public boolean runeIn(int monsterId, Rune r){
+        return this.get(monsterId).contains(r);
+    }
+}
